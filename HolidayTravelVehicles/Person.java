dba01f7962dffbe8be01c94736cf116b3bca5ec3
pphum-prathum <phanphump2004@gmail.com>
2025-02-20 21:25:45 +0700

    Create Person.java

---
+++ HolidayTravelVehicles/Person.java
@@ -0,0 +1,15 @@
+class Person {
+    private String name;
+    private String address;
+    private String phoneNo;
+
+    public Person(String name, String address, String phoneNo) {
+        this.name = name;
+        this.address = address;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getPhoneNo() { return phoneNo; }
+}
